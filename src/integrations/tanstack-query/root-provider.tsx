410d283a389fb82a9a3264c21d5280f2a258b62c
Ali <ali.hussein.pre@gmail.com>
2025-11-28 09:42:54 +0100

    install posthog

--- src/integrations/tanstack-query/root-provider.tsx
+++ src/integrations/tanstack-query/root-provider.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { PostHogProvider } from "posthog-js/react";
 
 export function getContext() {
   const queryClient = new QueryClient()
@@ -15,6 +16,18 @@ export function Provider({
   queryClient: QueryClient
 }) {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={{
+        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+        defaults: '2025-05-24',
+        capture_exceptions: true, // This enables capturing exceptions using Error Tracking
+        debug: import.meta.env.MODE === "development",
+      }}
+    >
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    </PostHogProvider>
   )
 }
